814b32b35ec4bdf1a006177042069c26882be03d
elizabeth-krav <140917670+elizabeth-krav@users.noreply.github.com>
2023-10-13 23:14:35 +0300

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,6 +1,5 @@
-//calculator 
-
 const resultElement = document.getElementById('result')
+//console.log(resultElement.textContent)
 resultElement.textContent = 42
 const num1 = document.getElementById('num1')
 const num2 = document.getElementById('num2')
@@ -16,7 +15,7 @@ function printResult(result) {
 			resultElement.style.color = 'red'
 	} else {
 			resultElement.style.color = 'green'
-	} 
+	}
 	resultElement.textContent = result
 }
 
@@ -36,7 +35,6 @@ divideBtn.onclick = function() {
 	action = '/'
 }
 
-
 function counting(inp1, inp2, actionSingn) {
 	const num1 = Number(inp1.value)
 	const num2 = Number(inp2.value)
@@ -48,7 +46,9 @@ function counting(inp1, inp2, actionSingn) {
 		return num1 * num2
 	} else if (actionSingn == '/') {
 		return num1 / num2
-	}		
+	}
+
+		
 }
 
 
@@ -56,3 +56,5 @@ submitBtn.onclick = function() {
 	const result = counting(num1, num2, action)
 	printResult(result)
 }
+
+//console.log(typeof sum)
